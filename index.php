13ef20f93c56f228ece4d3cdde633962651755f4
Oliver Kean L. Palgue <98378847+oliverkean@users.noreply.github.com>
2022-06-13 12:44:53 +0800

    Update and rename research1_Palgue.php to index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active text-light" href="./research1_Palgue.php">Home</a>
+            <a class="nav-link active text-light" href="./index.php">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link text-light" href="./regular_expression.php">Regular Expressions</a>
